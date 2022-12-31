396b119017f7210235fa1e5849157fd1a999643d
Hayden <115202535+haydenkress@users.noreply.github.com>
2022-12-31 17:58:53 -0500

    Complete removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
-
+    let array = Array.from(arguments[0]);
+    let itemsToRemove = Array.from(arguments).slice(1);
+    let newArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (!itemsToRemove.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    } 
+    return newArray;
 };
+console.log(removeFromArray([1, 2, 3, 4]), 1);
 
 // Do not edit below this line
 module.exports = removeFromArray;
