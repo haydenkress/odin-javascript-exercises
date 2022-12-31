8a87180b14fb387939f1f6c38e07ccbb6510f306
Hayden <115202535+haydenkress@users.noreply.github.com>
2022-12-31 15:17:18 -0500

    Complete repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let concatString = '';
+    if (number < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < number; i++) { 
+        concatString += String(string);
+    }
+    return concatString;
 };
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
