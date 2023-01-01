41bdeab6e50ea051ccd6f3289fd01a48de7ed638
Hayden <115202535+haydenkress@users.noreply.github.com>
2022-12-31 19:17:21 -0500

    Complete sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
 const sumAll = function() {
-
+    let number = arguments[0];
+    let endingNum = arguments[1];
+    if (!Number.isInteger(number) || !Number.isInteger(endingNum)) return "ERROR";
+    if (number < 0 || endingNum < 0) return "ERROR";
+    if (number > endingNum) {
+        const temp = number;
+        number = endingNum;
+        endingNum = temp;
+      }
+    let sum = 0;
+    for (let i = number; i <= endingNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
