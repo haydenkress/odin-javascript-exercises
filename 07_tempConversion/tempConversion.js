a2e8a776fc8b9b2f4434db270e8024225cc67fb1
Hayden <115202535+haydenkress@users.noreply.github.com>
2022-12-31 22:24:34 -0500

    Complete tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,32 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let calculation = (Number(degrees) - 32) * (5 / 9);
+  let celsius;
+  if (calculation === 0) {
+    celsius = Number(calculation.toFixed(0));
+  }
+  else {
+    celsius = Number(calculation.toFixed(1));
+  }
+  
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let calculation = Number(degrees) * (9 / 5) + 32;
+  let fahrenheit;
+  let freezing = calculation.toString().startsWith("32");
+
+  if (calculation === 0) {
+    fahrenheit = Number(calculation.toFixed(0));
+  } 
+  else if (freezing == true) {
+    fahrenheit = 32;
+  } 
+  else {
+    fahrenheit = Number(calculation.toFixed(1));
+  }
+  
+  return fahrenheit;
 };
 
 // Do not edit below this line
