26eb1c48b0fc60432a2802b5cc66d88abf15abf6
Hayden <115202535+haydenkress@users.noreply.github.com>
2022-12-31 21:56:40 -0500

    Complete leapYears tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 4 == 0 && year % 100 == 0) {
+            if (year % 400 == 0 && year % 100 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
+
+console.log(leapYears(2004));
 
 // Do not edit below this line
 module.exports = leapYears;
